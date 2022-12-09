eb27852ca0fd5d504f50cbc48bda0ed2e6af15a8
Kay Khine Pyone Wai <kaykhinepyonewai@gmail.com>
2022-12-09 16:05:49 +0630

    Add Complete Tutorial 5

--- Tutorial5/SQLQuery5.sql
+++ Tutorial5/SQLQuery5.sql
@@ -1,1 +1,4 @@
-select m.FullNames,m.Address,r.MovieRented,s.Salutation from Member as m,Rent as r,Salutation as s where m.MemberId=r.MemberId and m.SalutationId=s.SalutationId
+select * from cats
+select DISTINCT name From cats
+select * from dogs 
+select DISTINCT name From dogs
