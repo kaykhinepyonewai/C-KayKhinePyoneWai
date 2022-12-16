d73fadab259dc3ca5f8ccf922c57d9d836b21125
Kay Khine Pyone Wai <kaykhinepyonewai@gmail.com>
2022-12-16 12:33:55 +0630

    Add Tutorial9

---
+++ Tutorial9/Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Tutorial9._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+   
+   <div>
+       <h1 style="text-align:center">Importing Text & Excel & CSV File To GridView</h1>
+
+   </div>
+
+    <br />
+    <table class="nav-justified">
+        <tr>
+            <td style="width: 355px; height: 42px;">
+                <asp:FileUpload ID="FileUploadTo" runat="server" Width="305px" />
+                <br />
+            </td>
+            <td style="width: 498px; height: 42px;"><asp:Button ID="btnUpload" CssClass="btn btn-info" runat="server" Text="Upload File" OnClick="btnUpload_Click" style="margin-left: 20" /></td>
+            <td style="height: 42px"></td>
+        </tr>
+        <tr>
+            <td style="width: 355px; height: 20px">
+                <asp:Label ID="lblMsg" runat="server" Text="" Visible="False" ForeColor="#FF3300"></asp:Label>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="FileUploadTo" ValidationGroup="" ErrorMessage="Please Choose Text file || Excel File || CSV File" ForeColor="#FF3300"></asp:RequiredFieldValidator>
+            </td>
+            <td style="width: 498px; height: 20px"></td>
+            <td style="height: 20px"></td>
+        </tr>
+        <tr>
+            <td style="width: 355px">&nbsp;</td>
+            <td style="width: 498px"><asp:GridView ID="GridView1" runat="server" Width="99%" CellPadding="4" ForeColor="#333333" GridLines="None">
+                <AlternatingRowStyle BackColor="White" />
+                <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+                <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+                <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+                <SortedAscendingCellStyle BackColor="#FDF5AC" />
+                <SortedAscendingHeaderStyle BackColor="#4D0000" />
+                <SortedDescendingCellStyle BackColor="#FCF6C0" />
+                <SortedDescendingHeaderStyle BackColor="#820000" />
+                </asp:GridView>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+    </table>
+</asp:Content>
---
+++ Tutorial9/Default.aspx.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Configuration;
+using System.IO;
+using System.Data;
+using OfficeOpenXml;
+using System.Web.Services.Discovery;
+using System.Net;
+
+namespace Tutorial9
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            string FileExt = System.IO.Path.GetExtension(FileUploadTo.PostedFile.FileName);
+            if (FileExt == ".xls" || FileExt == ".xlsx")
+            {
+                lblMsg.Visible = false;
+                excelUpload();
+            }
+            else if(FileExt == ".csv")
+            {
+                lblMsg.Visible = false;
+                csvUpload();
+            }
+            else if(FileExt == ".txt")
+            {
+                lblMsg.Visible = false;
+                txtUpload();
+            }
+            else
+            {
+                lblMsg.Visible = true;
+                lblMsg.Text = "Please Choose Text file || Excel File || CSV File";
+            }
+        }
+
+        void txtUpload()
+        {
+          
+            string txtPath = Server.MapPath("~/Datas/") + Path.GetFileName(FileUploadTo.PostedFile.FileName);
+            FileUploadTo.SaveAs(txtPath);
+
+          
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[5]
+            {
+            new DataColumn("Id", typeof(int)),
+            new DataColumn("Name", typeof(string)),
+            new DataColumn("Address",typeof(string)),
+            new DataColumn("PhoneNo",typeof(string)),
+            new DataColumn("GmailAddress",typeof(string)),
+            });
+
+
+            string csvData = File.ReadAllText(txtPath);
+
+           
+            foreach (string row in csvData.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    dt.Rows.Add();
+                    int i = 0;
+                    foreach (string cell in row.Split(' '))
+                    {
+                        dt.Rows[dt.Rows.Count - 1][i] = cell;
+                        i++;
+                    }
+                }
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+         void csvUpload()
+        {
+            string csvPath = Server.MapPath("~/Datas/") + Path.GetFileName(FileUploadTo.PostedFile.FileName);
+            FileUploadTo.SaveAs(csvPath);
+            DataTable dt = new DataTable();
+
+            dt.Columns.AddRange(new DataColumn[5]
+            {
+            new DataColumn("Id", typeof(int)),
+            new DataColumn("Name", typeof(string)),
+            new DataColumn("Address",typeof(string)),
+            new DataColumn("PhoneNo",typeof(string)),
+            new DataColumn("GmailAddress",typeof(string)),
+            });
+
+            string csvData = File.ReadAllText(csvPath);
+            foreach (string row in csvData.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    dt.Rows.Add();
+                    int i = 0;
+                    foreach (string cell in row.Split(','))
+                    {
+                        dt.Rows[dt.Rows.Count - 1][i] = cell;
+                        i++;
+                    }
+                }
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+
+        void excelUpload()
+        {
+            string FilePath = ConfigurationManager.AppSettings["FilePath"].ToString();
+            string filename = string.Empty;
+            if (FileUploadTo.HasFile)
+            {
+
+                string[] allowdFile = { ".xls", ".xlsx" };
+                string FileExt = System.IO.Path.GetExtension(FileUploadTo.PostedFile.FileName);
+                bool isValidFile = allowdFile.Contains(FileExt);
+                if (!isValidFile)
+                {
+                    lblMsg.ForeColor = System.Drawing.Color.Red;
+                    lblMsg.Text = "Please upload only Excel";
+                }
+                else
+                {
+                    int FileSize = FileUploadTo.PostedFile.ContentLength;
+                    if (FileSize <= 1048576)
+                    {
+                        filename = Path.GetFileName(Server.MapPath(FileUploadTo.FileName));
+                        FileUploadTo.SaveAs(Server.MapPath(FilePath) + filename);
+                        string filePath = Server.MapPath(FilePath) + filename;
+
+                        DataTable dt = new DataTable();
+                        var existingFile = new FileInfo(filePath);
+                        using (var package = new ExcelPackage(existingFile))
+                        {
+                            ExcelWorkbook workBook = package.Workbook;
+                            if (workBook != null)
+                            {
+                                if (workBook.Worksheets.Count > 0)
+                                {
+                                    ExcelWorksheet worksheet = workBook.Worksheets.First();
+                                    ExcelCellAddress startCell = worksheet.Dimension.Start;
+                                    ExcelCellAddress endCell = worksheet.Dimension.End;
+
+
+                                    for (int col = startCell.Column; col <= endCell.Column; col++)
+                                    {
+                                        object col1Header1 = worksheet.Cells[1, col].Value;
+                                        dt.Columns.Add("" + col1Header1 + "");
+                                    }
+                                    for (int row = startCell.Row + 1; row <= endCell.Row + 1; row++)
+                                    {
+                                        DataRow dr = dt.NewRow();
+                                        int x = 0;
+                                        for (int col = startCell.Column; col <= endCell.Column; col++)
+                                        {
+                                            dr[x++] = worksheet.Cells[row, col].Value;
+                                        }
+                                        dt.Rows.Add(dr);
+                                        GridView1.DataSource = dt;
+                                        GridView1.DataBind();
+                                    }
+                                }
+
+                            }
+
+                        }
+
+                    }
+                }
+            }
+
+
+        }
+    }
+    }
+    
